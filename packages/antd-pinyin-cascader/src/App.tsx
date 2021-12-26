7f9a860cb1dddc8493f34898626b772af5a687b5
86XIng <948290581@qq.com>
2021-12-26 11:09:13 +0800

    Cascader highlight

--- packages/antd-pinyin-cascader/src/App.tsx
+++ packages/antd-pinyin-cascader/src/App.tsx
@@ -6,8 +6,8 @@ import type {
     FieldNames,
     DataNode,
 } from 'rc-cascader/lib/interface';
-import {pinYinFuzzSearch} from '@dpdfe/tools';
-import PinyinMatch from 'pinyin-match';
+import {pinyinFuzzySearchAdvance, pinYinFuzzSearch} from '@dpdfe/tools';
+
 import 'antd/es/cascader/style/index';
 
 import './App.css';
@@ -15,46 +15,40 @@ import {CascaderRef} from 'antd/lib/cascader';
 
 function highlightKeyword(
     str: string,
-    lowerKeyword: string,
+    position: number[],
     prefixCls: string | undefined,
 ) {
-    const cells = str
-        .toLowerCase()
-        .split(lowerKeyword)
-        .reduce(
-            (list, cur, index) =>
-                index === 0 ? [cur] : [...list, lowerKeyword, cur],
-            [],
-        );
     const fillCells: React.ReactNode[] = [];
-    let start = 0;
 
-    cells.forEach((cell, index) => {
-        const end = start + cell.length;
-        let originWorld: React.ReactNode = str.slice(start, end);
-        start = end;
-
-        if (index % 2 === 1) {
-            originWorld = (
+    for (let index = 0; index < str.length; index++) {
+        let originWord: React.ReactNode = str[index];
+        if (position.includes(index)) {
+            originWord = (
                 <span
                     className={`${prefixCls}-menu-item-keyword`}
-                    key="seperator"
+                    key={str + index}
                 >
-                    {originWorld}
+                    {originWord}
                 </span>
             );
         }
-
-        fillCells.push(originWorld);
-    });
+        fillCells.push(originWord);
+    }
 
     return fillCells;
 }
 
 let path_list: DataNode[][] = [];
-let match_list = new Map<string, Map<string, boolean>>();
+let match_list = new Map<
+    string,
+    [Map<string, boolean>, Map<string, Array<number>>]
+>();
+let match_position = new Map<string, Array<number>>();
+const single_match_position = new Map<string | number, Array<number>>();
+let single_match_num = 0;
 let has_path_list = false;
 
+//TODO: 输入全拼时高亮有问题
 const defaultSearchRender: ShowSearchType['render'] = (
     inputValue,
     path,
@@ -62,62 +56,119 @@ const defaultSearchRender: ShowSearchType['render'] = (
     fieldNames,
 ) => {
     const optionList: React.ReactNode[] = [];
+    const now = performance.now();
 
     // We do lower here to save perf
     const lower = inputValue.toLowerCase();
 
-    has_path_list = true;
+    if (!has_path_list) {
+        path.forEach((node, index) => {
+            if (index !== 0) {
+                optionList.push(' / ');
+            }
 
-    path.forEach((node, index) => {
-        if (index !== 0) {
-            optionList.push(' / ');
+            let label = (node as any)[fieldNames.label!];
+            const type = typeof label;
+            if (type === 'string' || type === 'number') {
+                // if (single_match_position.has(label)) {
+                //     label = highlightKeyword(
+                //         String(label),
+                //         single_match_position.get(label)!,
+                //         prefixCls,
+                //     );
+                // }
+            }
+
+            optionList.push(label);
+        });
+
+        if (single_match_num !== 1) {
+            single_match_num--;
+        } else {
+            has_path_list = true;
         }
+    } else {
+        let is_valid_label = true;
+        let label: any = path.reduce((pre, cur, index) => {
+            let label = (cur as any)[fieldNames.label!];
+            const type = typeof label;
+            if (type === 'string' || type === 'number') {
+                return index === 0 ? label : pre + ' / ' + label;
+            } else {
+                is_valid_label = false;
+                optionList.push(label);
+            }
 
-        let label = (node as any)[fieldNames.label!];
-        const type = typeof label;
-        if (type === 'string' || type === 'number') {
-            label = highlightKeyword(String(label), lower, prefixCls);
-        }
+            return pre;
+        }, '');
 
+        // label = highlightKeyword(
+        //     String(label),
+        //     match_position.get(label)!,
+        //     prefixCls,
+        // );
         optionList.push(label);
-    });
+    }
+
     return optionList;
 };
 
 let searchConfig: ShowSearchType = {
     render: defaultSearchRender,
-    filter(input, path) {
+    filter(input, path, fieldNames) {
         if (!has_path_list) {
             // 未缓存过全部path_list
-            const res = pinYinFuzzSearch(input, path, {
+            const res = pinyinFuzzySearchAdvance(input, path, {
                 textProvider(dn) {
                     return dn.label!.toString();
                 },
             });
 
+            for (const [key, value] of res.position) {
+                let label = (key as any)[fieldNames.label!];
+                const type = typeof label;
+                if (type === 'string' || type === 'number') {
+                    single_match_position.set(label, value);
+                }
+            }
+
             path_list.push(path);
-            return res.length === 0;
+            if (res.result.length > 0) {
+                single_match_num++;
+            }
+
+            return res.result.length !== 0;
         }
 
         if (!match_list.has(input)) {
             // 没有input的缓存
-            const now = performance.now();
             const match_result = new Map<string, boolean>();
 
-            const res = pinYinFuzzSearch(input, path_list, {
+            const res = pinyinFuzzySearchAdvance(input, path_list, {
                 textProvider(dn) {
-                    return dn.map((item) => item.label).join('');
+                    return dn.map((item) => item.label).join(' / ');
                 },
             });
-            res.forEach((item) => {
+            for (const [key, value] of res.position) {
+                match_position.set(
+                    key.map((k) => k.label?.toString()).join(' / '),
+                    value,
+                );
+            }
+
+            res.result.forEach((item) => {
                 match_result!.set(item.map((i) => i.label).join('-'), true);
             });
 
-            console.log(performance.now() - now);
-            match_list.set(input, match_result);
+            match_list.set(input, [
+                new Map(match_result),
+                new Map(match_position),
+            ]);
         }
 
-        const match_result = match_list.get(input);
+        const [match_result, _match_position] = match_list.get(input)!;
+
+        match_position = new Map(_match_position);
 
         const string_path = path.map((i) => i.label).join('-');
 
@@ -126,6 +177,7 @@ let searchConfig: ShowSearchType = {
     limit: Infinity,
 };
 
+//TODO: 卡顿
 function App() {
     return (
         <Cascader
